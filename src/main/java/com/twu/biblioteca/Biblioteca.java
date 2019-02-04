bc8d65dd33375e9f9130b176b61cd07795802e90
jutorres <torresjujuliana@gmail.com>
2019-02-04 12:20:41 -0200

    1.2 - View a list of all books

--- src/main/java/com/twu/biblioteca/Biblioteca.java
+++ src/main/java/com/twu/biblioteca/Biblioteca.java
@@ -1,8 +1,19 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Biblioteca {
+
+    private List<String> bookList = new ArrayList<>();
 
     public String welcomeMessage() {
         return "Welcome to Biblioteca! Your one-stop-shop for great book titles in Bangalore!";
+    }
+
+    public List<String> getBookList() {
+        bookList = Arrays.asList("Dom Casmurro", "Senhora", "Memorias Postumas de Bras Cubas");
+        return bookList;
     }
 }
--- src/test/java/com/twu/biblioteca/ExampleTest.java
+++ src/test/java/com/twu/biblioteca/ExampleTest.java
@@ -4,6 +4,9 @@ package com.twu.biblioteca;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -24,6 +27,13 @@ public class ExampleTest {
         assertThat("Welcome to Biblioteca! Your one-stop-shop for great book titles in Bangalore!", is(expected));
     }
 
+    @Test
+    public void testIfTheBookListIsShowing() {
 
+        List<String> actual = Arrays.asList("Dom Casmurro", "Senhora", "Memorias Postumas de Bras Cubas");
+        List<String> expected = biblioteca.getBookList();
+
+        assertThat(actual, is(expected));
+    }
 
 }
