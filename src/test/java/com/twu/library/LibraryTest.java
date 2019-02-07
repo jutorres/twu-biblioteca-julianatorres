516bc3b6527ed59af8b96f9eb20ada9af50e4a1b
jutorres <torresjujuliana@gmail.com>
2019-02-07 14:51:15 -0200

    Add unit test to this function #9

--- src/test/java/com/twu/library/LibraryTest.java
+++ src/test/java/com/twu/library/LibraryTest.java
@@ -10,6 +10,8 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -22,11 +24,6 @@ public class LibraryTest {
 
     @Before
     public void setup() {
-        library = new Library(bookList, mockPrintStream);
-    }
-
-    @Test
-    public void testIfOnlyAvailableBooksAppearsInList() {
         Book book1 = new Book("Dom Casmurro", "Machado de Assis", 1900, BookStatus.AVAILABLE);
         Book book2 = new Book("Senhora", "Jose de Alencar", 1900, BookStatus.AVAILABLE);
         Book book3 = new Book("Memorias Postumas de Bras Cubas", "Machado de Assis", 1900, BookStatus.UNAVAILABLE);
@@ -36,11 +33,25 @@ public class LibraryTest {
         bookList.add(book1);
         bookList.add(book2);
         bookList.add(book3);
+        library = new Library(bookList, mockPrintStream);
+    }
+
+    @Test
+    public void testIfOnlyAvailableBooksAppearsInList() {
 
         library.getAvailableBookList();
 
         verify(mockPrintStream).println(bookList.get(0));
         verify(mockPrintStream, times(0)).println(bookList.get(2));
+
+    }
+
+    @Test
+    public void testIfReturnAnSpecificBookFromListSelectedByCostumer() {
+
+        Book selectedBook = bookList.get(0);
+
+        assertThat(selectedBook, equalTo(library.getAnSpecificBookFromListSelectedByCostumer(1)));
 
     }
 
